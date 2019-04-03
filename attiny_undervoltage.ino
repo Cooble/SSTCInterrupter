d14aac464fd80dc3c547a62a608dcb9cfcd2e2db
Cobble <minekoza@gmail.com>
2019-04-03 16:34:45 +0200

    Update attiny_undervoltage.ino

--- attiny_undervoltage.ino
+++ attiny_undervoltage.ino
@@ -1,3 +1,7 @@
+/**
+Runs on attiny, prevents sstc circuit from shorting while switching off (capacitors slowly discharge)
+Cuts power to circuit when below some threshold value
+*/
 void setup() {
   pinMode(0, OUTPUT);
   analogReference(INTERNAL);
