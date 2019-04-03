94d0112f34f116b100f5d3d1777b8f58c445c39f
Cobble <minekoza@gmail.com>
2019-04-03 16:32:46 +0200

    Add files via upload
    Runs on tiny, prevents from shorts during switching off the coil

---
+++ attiny_undervoltage.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(0, OUTPUT);
+  analogReference(INTERNAL);
+  delay(2000);
+}
+
+void loop() {
+  uint16_t  i = analogRead(2);
+  digitalWrite(0, i >= 890);
+  if(!(i >= 890)){
+    delay(5000);
+  }
+}
